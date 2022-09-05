243e7baf43846dcd1d9c1b0f8326bbca12e6e86a
jidi1f <chernyshdanil106@gmail.com>
2022-09-06 02:54:41 +0300

    feat: implement XenCrypt

---
+++ app/src/main/java/utils/Errors.kt
@@ -0,0 +1,12 @@
+package utils
+
+class DecryptionError(message: String?) : Exception("Failed to decrypt: $message")
+
+class EncryptionError(message: String?) : Exception("Failed to encrypt: $message")
+
+class SessionIdError(message: String?) :
+    Exception("Failed to generate session id: $message")
+
+class WrongPublicKeyError(message: String?) :
+    Exception("Something happen while decoding publicKey: $message")
+
